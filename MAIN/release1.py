c92677abf509f6487f4ed5f3a76a34b6a2971d6a
Void-STUDIOSDEV <voiddevr@protonmail.com>
2026-04-15 14:35:46 -0400

    RELEASE ME!
    I was fed up with the way I was finding how much storage I was. Opening a file manager application or running fastfetch/neofetch was redundant. And depending on the file manager, heavy. So I made a lightweight CLI/TUI Application that can even work in TTY!

---
+++ MAIN/release1.py
@@ -0,0 +1,45 @@
+import shutil
+import time
+import sys
+sleep = time.sleep
+#required modules
+
+
+sleep(0.5); print("Linux Disk Reader, LDR")
+sleep(0.5); print("Version 01.00.00")
+sleep(0.5); print("COPYRIGHT VOID STUDIOS 2026")
+sleep(0.5); print("COVERED UNDER GNU General Public License 2.x")
+sleep(0.5); print("E TO EXIT, S TO LIST [START]")
+sleep(0.5); print("R TO LIST WITHOUT PROGRAM ENDING [MANUAL ENDING]")
+#introduction
+
+
+while True:
+	choice = input("CHOICE: ").upper()
+	if choice == "E":
+		print("Goodbye!\n")
+		break
+	#allows the user to exit
+		
+	if choice == "S":
+		sleep(0.7); print("LISTING...")
+		total, used, free = shutil.disk_usage(".")
+		sleep(0.5); print("TOTAL: ", total / (1024**3), "GB")
+		sleep(0.5); print("USED: ", used / (1024**3), "GB")
+		sleep(0.5); print("FREE: ", free / (1024**3), "GB\n")
+		break
+	#prints out disk informations and automatically ends	
+		
+	if choice == "R":
+		sleep(0.7); print("LISTING...")
+		total, used, free = shutil.disk_usage(".")
+		sleep(0.5); print("TOTAL: ", total / (1024**3), "GB")
+		sleep(0.5); print("USED: ", used / (1024**3), "GB")
+		sleep(0.5); print("FREE: ", free / (1024**3), "GB\n")
+		continue
+	#prints out disk informations. manual ending required
+		
+	if choice not in ["E", "S", "R"]:
+		print("INVALID\n")
+		continue
+	#invalid input handling
